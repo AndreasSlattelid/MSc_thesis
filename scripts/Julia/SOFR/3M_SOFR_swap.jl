dd9fc395c4a3614344031e742998b2d108a98107
Andreas Slåttelid <87479787+AndreasSlattelid@users.noreply.github.com>
2023-03-22 10:26:37 +0100

    changed chapter structure of SOFR-swap, working on finding parameters Vasicek from observed ZCB-data

---
+++ scripts/Julia/SOFR/3M_SOFR_swap.jl
@@ -0,0 +1,187 @@
+#statistics and distributions
+using Random 
+using Distributions
+using Statistics
+
+#data-wrangeling:
+using DataFrames
+
+#for numerical integration:
+using QuadGK
+
+#plotting histogram and LaTeX labels:
+using Plots 
+using LaTeXStrings
+
+#---------------------------------------------------------------------
+#time parameters
+T0 = 1/12
+T1 = 4/12
+T2 = 7/12
+T3 = 10/12
+timepoints = [T0, T1, T2, T3]
+n_steps = 10000
+
+#We use that Vasicek is ATS, i.e P(t,T) = exp(-A(t,T)-B(t,T)r(t))
+
+function r_Vasicek(alpha, m,sigma, r_t, time_interval, n_steps)
+    """
+    Args: 
+        #Vasicek parameters:
+        alpha (Float): speed of reversion 
+        m (float): long term mean level 
+        sigma (float): volatility 
+        r_t (float): initial value of r = (r(u))
+        
+        #time:
+        time_interval (vector): the time interval we model over e.g [0,3]
+        n_steps (int): number of timesteps we partition over
+    
+    Returns: 
+        it simulates the process: r = (r(u)), for u in [t_start, t_end]
+    """
+
+    #time partition: 
+    t_start = time_interval[1]
+    t_end = time_interval[2]
+    dt = (t_end-t_start)/n_steps
+    n_steps = length(collect(t_start:dt:t_end))
+
+    #initializing r: 
+    r = zeros(n_steps)
+    r[1] = r_t
+
+    #Standard normal rv's
+    Z = rand(Normal(0,1), n_steps)
+
+    for i in 2:n_steps
+        r[i] = r[i-1] - alpha*(m-r[i-1])*dt + sigma*Z[i]*dt
+    end
+
+    return r
+end
+
+function B_ZCB(t,T)
+    ans = -(1/alpha)*(exp(-alpha*(T-t))-1)
+    return ans
+end
+
+function A_ZCB(t,T)
+    integral, _ = quadgk(u -> B_ZCB(u,T)^(2), t,T)
+    ans = m*B_ZCB(t,T) - m*(T-t) - (1/2)*sigma^(2)*integral 
+    return ans
+end
+
+function P(t,T, r_t)
+    ans = exp(-A_ZCB(t,T) -B_ZCB(t,T)*r_t)
+    return ans
+end
+
+#--------------------------------------------------------------
+#Calculating f^3M(t,S,T), again using ATS structure:
+#f^3M(t,S,T) = 1/(T-S)*(exp(A(t,S,T) + B(t,S,T)r(t))-1)
+function Sigma1(u,t,S,T)
+    ans = exp(-alpha*(S-u)) - exp(-alpha*(T-u))
+    return ans   
+end
+
+function Sigma2(u,t,S,T)
+    ans = 1-exp(-alpha*(T-u))
+    return ans
+end
+
+function B(t,S,T)
+    ans = (1/alpha)*(exp(-alpha*(S-t))-exp(-alpha*(T-t)))
+    return ans
+end
+
+function A(t,S,T)
+    first_part = m*(T-S) - m*B(t,S,T)
+    c1_2, _  = quadgk(u -> Sigma1(u, t, t, S)^(2), t,S) 
+    c2_2, _  = quadgk(u -> Sigma2(u, t, S, T)^(2), S,T)
+
+    ans = first_part + (1/2)*(sigma^2/alpha^2)*(c1_2 + c2_2)
+
+    return ans
+end
+
+
+function f_3M(t,S,T, r_t)
+    ans = (1/(T-S))*(exp(A(t,S,T) + B(t,S,T)*r_t) - 1)
+    return ans
+end 
+
+#time t-value of kappa in SOFR-swap
+function kappa_t(t, r_t)
+    "
+    Args: 
+        t (float): a vector of time points i.e [0,T1]
+        r_t (float): vector of realization of interest rate model
+    Returns: 
+        the fixed swap rate kappa_t_3M-SOFR
+    "
+    ZCB_prices = map(T -> P(t,T, r_t), timepoints[2:end])
+    f_3M_rates = map((x, y) -> f_3M(t, x, y, r_t), timepoints[1:end-1], timepoints[2:end])
+    above = sum(ZCB_prices.*f_3M_rates)
+    below = sum(ZCB_prices) 
+    ans = above/below
+    return ans
+end
+
+
+function kappa_path(t, r)
+    "
+    Args: 
+        t (float): vector of timepoints 
+        r (float): vector of desired interest rate model
+    Returns: 
+        vector of (t,r_t)-valued kappa_t
+    "
+    ans = map((x, y) -> kappa_t(x, y), t, r)
+    return ans
+end
+
+#Vasicek parameters: 
+alpha = 0.25
+m = 0.035
+r_0 = 0.0425
+sigma = 0.05
+
+r_0 = 0.0425
+t_start = 0 
+t_end = T0
+
+r = r_Vasicek(alpha, m, sigma, r_0, [t_start,t_end], n_steps)
+dt = (t_end-t_start)/n_steps
+t = collect(t_start:dt:t_end)
+
+length(r)
+n_sim = 10
+M = zeros(length(r), n_sim)
+R = zeros(length(r), n_sim)
+
+
+Random.seed!(1234)
+for i in 1:n_sim
+    r = r_Vasicek(alpha, m, sigma, r_0, [t_start,t_end], n_steps)
+    kappa = kappa_path(t,r)
+    R[:, i] = r
+    M[:, i] = kappa
+end
+M
+R
+
+plot(R, layout = (1,1), 
+        legend = false,
+        title = L"t \mapsto r(t),\alpha = 0.25, m = 0.035, \sigma = 0.05, r_{0} = 0.0425 "
+        )
+xticks!([0, 10_000/2 ,10_000], ["0", L"\frac{T_{0}}{2}", L"T_{0}"])
+
+
+# plot each column of the matrix
+plot(M, layout=(1,1), 
+        legend = false, 
+        title = L"t \mapsto \kappa_{t}^{3M-SOFR},\alpha = 0.25, m = 0.035, \sigma = 0.05, r_{0} = 0.0425"
+        )
+xticks!([0, 10_000/2 ,10_000], ["0", L"\frac{T_{0}}{2}", L"T_{0}"])
+
---
+++ scripts/Julia/SOFR/SOFR_hedge_f3M_ATS.jl
@@ -0,0 +1,231 @@
+using Random 
+using Distributions
+using Statistics
+
+#for matrix operations and linear programming
+using LinearAlgebra
+using JuMP  #lp-problem setup
+using HiGHS #lp-solver
+
+#data-wrangeling:
+using DataFrames
+
+#for numerical integration:
+using QuadGK
+
+#plotting histogram and LaTeX labels:
+using Plots 
+using LaTeXStrings
+
+#--------------------------------------------------------------------------#
+#Vasicek parameters: 
+alpha = 0.25
+m = 0.035
+r_t = 0.0425
+sigma = 0.02
+
+#time parameters
+t = 0
+S = 1/12
+T1M = 2/12
+T2M = 3/12
+T = 4/12
+
+function Sigma1(u,t,S,T)
+    ans = exp(-alpha*(S-u)) - exp(-alpha*(T-u))
+    return ans   
+end
+
+
+function Sigma2(u,t,S,T)
+    ans = 1-exp(-alpha*(T-u))
+    return ans
+end
+
+
+function int_r_start_stop(low,up, t)
+    "
+    Args: 
+        low: (float), lower integration limit
+        up: (float), upper integtation limit
+    Returns: 
+        the integral: int_low_up r(u)du
+    "
+    if t > low
+        return "Please chose t <=low"
+    end
+
+    #Sigma1 is N(0, int_t_low c1_2 du), Sigma2 is N(0, int_low_up c2_2 du)
+    c1_2, _  = quadgk(u -> Sigma1(u, t, low,up)^(2), t,low) 
+    c2_2, _  = quadgk(u -> Sigma2(u, t, low,up)^(2), low,up)
+    
+    c1 = sqrt(c1_2)
+    c2 = sqrt(c2_2)
+    Z = rand(Normal(0,1))
+    ans = ((r_t-m)/alpha)*(exp(-alpha*(low-t))-exp(-alpha*(up-t))) + m*(up-low) + sigma/alpha*(c1*Z + c2*Z)
+    return ans
+end
+
+
+function integrand_E_Q_r(u, r_t, t)
+    ans = exp(-alpha*(u-t))*r_t + m*(1-exp(-alpha*(u-t)))
+    return ans
+end
+
+#int_S_T E_Q[r(u)|F_t]du:
+integral_E_Q_r, _ = quadgk(u -> integrand_E_Q_r(u, r_t, t), S,T)
+
+integral_E_Q_r
+#--------------------------------------------------------------------------#
+# Calculating a_hat_3M:
+
+function B(t,S,T)
+    ans = (1/alpha)*(exp(-alpha*(S-t))-exp(-alpha*(T-t)))
+    return ans
+end
+
+function A(t,S,T)
+    first_part = m*(T-S) - m*B(t,S,T)
+    c1_2, _  = quadgk(u -> Sigma1(u, t, t, S)^(2), t,S) 
+    c2_2, _  = quadgk(u -> Sigma2(u, t, S, T)^(2), S,T)
+
+    ans = first_part + (1/2)*(sigma^2/alpha^2)*(c1_2 + c2_2)
+
+    return ans
+end
+
+
+function f_3M(t,S,T)
+    ans = (1/(T-S))*(exp(A(t,S,T) + B(t,S,T)*r_t) - 1)
+    return ans
+end
+
+f_3M(0,S,T)
+
+a_hat = integral_E_Q_r/((T-S)*f_3M(0,S,T))
+
+#-------------------------------------------------------------------------------------------#
+# 3M-arithmetic vs (a,b,c) 1M-SOFR futures: 
+integral1 , _ = quadgk(u -> integrand_E_Q_r(u, r_t, t), S,T1M)
+integral2 , _ = quadgk(u -> integrand_E_Q_r(u, r_t, t), T1M,T2M)
+integral3 , _ = quadgk(u -> integrand_E_Q_r(u, r_t, t), T2M,T)
+
+f_1M_S_T1M = (1/(T1M-S))*integral1
+f_1M_T1M_T2M = (1/(T2M-T1M))*integral2
+f_1M_T2M_T = (1/(T-T2M))*integral3
+
+#variable naming to be more consistent with MSc Thesis:
+a = f_1M_S_T1M        #alpha, I use alpha in Vasicek, hence a: 
+beta = f_1M_T1M_T2M   #beta
+gamma = f_1M_T2M_T    #gamma
+
+futures = [a,beta,gamma]
+
+#E_Q[X^(3M_A)(S,T)|F_t] = q: 
+q = (1/(T-S))*integral_E_Q_r 
+
+#matrix of coeff: 
+M = [a^(2)      a*beta  a*gamma;
+     beta^(2)   a*beta  beta*gamma; 
+     gamma^(2)  a*beta  beta*gamma] 
+     
+#vector of values: 
+b = q.*[a;
+        beta;
+        gamma] 
+
+#optimal weight of futures:
+x_hat = inv(M)*b 
+#-------------------------------------------------------------------------------------------#
+# incase M is not invertible: 
+# Define optimization problem
+model = Model(HiGHS.Optimizer)
+@variable(model, x[1:3])
+@objective(model, Min, sum(x))
+@constraint(model, M * x .== b)
+@constraint(model, -1 .<= x .<= 1 )
+abs.([1,2,-3])
+# Solve optimization problem
+optimize!(model)
+# optimal value
+x_tilde = value.(x)
+
+#-------------------------------------------------------------------------------------------#
+# Simulations: 
+n_sim = 10^(6)
+#constants: 
+#int_S_T E_Q[r(u)|F_t]du:
+integral_E_Q_r, _ = quadgk(u -> integrand_E_Q_r(u, r_t, t), S,T)
+
+futures_weighted_M_inv = x_hat'futures
+futures_weighted_BP = x_tilde'futures
+futures_weighted_one_each = [1,1,1]'futures
+
+X_3MA = zeros(n_sim)
+for i in 1:n_sim
+    #aritmetic interest rate relaization:
+    X_3MA[i] = (1/(T-S))*(int_r_start_stop(S,T,t))
+end
+
+X_3MA
+#elementwise substraction:
+ER_1 = X_3MA .-(1/(T-S))*integral_E_Q_r
+ER_2_M_inv = X_3MA .-futures_weighted_M_inv
+ER_2_111 = X_3MA .-futures_weighted_one_each
+
+
+
+#-----------------------------------------------------------
+# plotting of histograms: 
+mean_ER_1 = mean(ER_1)
+mean_ER_1 = round(mean_ER_1, digits = 3)
+
+sigma_ER_1 = std(ER_1) 
+sigma_ER_1 = round(sigma_ER_1, digits = 2)
+
+mean_ER_2_M_inv = mean(ER_2_M_inv)
+mean_ER_2_M_inv = round(mean_ER_2_M_inv, digits = 3)
+
+sigma_ER_2 = std(ER_2_M_inv)
+sigma_ER_2 = round(sigma_ER_2_M_inv, digits = 2)
+
+mean_ER_2_111 = mean(ER_2_111)
+mean_ER_2_111 = round(mean_ER_2_111,digits =3)
+
+sigma_ER_2_111 = std(ER_2_111)
+sigma_ER_2_111 = round(sigma_ER_2_111, digits = 2)
+
+#ER_1
+histogram(ER_1, 
+          color =:lightblue, 
+          xlabel="Value", 
+          ylabel="Frequency", 
+          title = L"Histogram\; of\; ER_{1}(0), \; s_{ER_{1}}\approx 0.01,\;n_{sim} = 10^{6}", 
+          labels = "ER_1(0)", 
+          xticks = [-3*sigma_ER_1, -2*sigma_ER_1,-sigma_ER_1, mean_ER_1,  sigma_ER_1, 2*sigma_ER_1, 3*sigma_ER_1]
+          )
+vline!([mean_ER_1], lw = 5, labels = L"mean(ER_{1}(0))" )
+
+
+#ER_2_M_inv:
+histogram(ER_2_M_inv, 
+          color =:lightblue, 
+          xlabel="Value", 
+          ylabel="Frequency", 
+          title = L"Histogram\; of\; ER_{2}^{M_{inv}}(0), \; s_{ER_{2}^{M_{inv}}} \approx 0.01, \;n_{sim} = 10^{6}", 
+          labels = L"ER_{2}^{M_{inv}}(0)", 
+          xticks = [-3*sigma_ER_2, -2*sigma_ER_2,-sigma_ER_2, mean_ER_2_M_inv, sigma_ER_2, 2*sigma_ER_2, 3*sigma_ER_2]
+          )
+vline!([mean_ER_2_M_inv], lw = 5, labels = L"mean(ER_{2}^{M_{inv}}(0))")
+
+#(1,1,1)-weight of futures:
+histogram(ER_2_111, 
+          color =:lightblue, 
+          xlabel="Value", 
+          ylabel="Frequency", 
+          title = L"Histogram\; of\; ER_{2}^{(1,1,1)}(0), \; s_{ER_{2}^{(1,1,1)}} \approx 0.01, \;n_{sim} = 10^{6}", 
+          labels = L"ER_{2}^{(1,1,1)}(0)", 
+          xticks = [-12*sigma_ER_2_111,-10*sigma_ER_2_111, mean_ER_2_111, -7*sigma_ER_2_111, -5*sigma_ER_2_111])
+vline!([mean_ER_2_one_each], lw = 5, labels =  L"mean(ER_{2}^{(1,1,1)}(0))")
+
+
